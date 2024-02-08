c13df4b8eb3f4827894af45d4a5e0afa48051037
sun_cs <nick225599@163.com>
2024-02-08 20:29:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app">
+        <strong>你好啊！</strong>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style>
+ .app{
+    background-color: aliceblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+ }
+</style>
